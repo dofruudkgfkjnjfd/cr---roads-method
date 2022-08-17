df24bbbc2249c29829a391037aff2ac1c529589d
youngchongosreal <100457390+youngchongosreal@users.noreply.github.com>
2022-08-17 12:33:13 -0400

    Update leaked-cr---roads-methods.lua

--- leaked-cr---roads-methods.lua
+++ leaked-cr---roads-methods.lua
@@ -1,17 +1,5 @@
 --[[
-  _                _            _    _____                                       _       ____                __  __      _   _               _ 
- | |              | |          | |  / ____|                                     | |     |  _ \              |  \/  |    | | | |             | |
- | |     ___  __ _| | _____  __| | | |     _ __ ___  ___ ___ _ __ ___   __ _  __| |___  | |_) | __ _ _ __   | \  / | ___| |_| |__   ___   __| |
- | |    / _ \/ _` | |/ / _ \/ _` | | |    | '__/ _ \/ __/ __| '__/ _ \ / _` |/ _` / __| |  _ < / _` | '_ \  | |\/| |/ _ \ __| '_ \ / _ \ / _` |
- | |___|  __/ (_| |   <  __/ (_| | | |____| | | (_) \__ \__ \ | | (_) | (_| | (_| \__ \ | |_) | (_| | | | | | |  | |  __/ |_| | | | (_) | (_| |
- |______\___|\__,_|_|\_\___|\__,_|  \_____|_|  \___/|___/___/_|  \___/ \__,_|\__,_|___/ |____/ \__,_|_| |_| |_|  |_|\___|\__|_| |_|\___/ \__,_|
-                                                                                                                                               
-I've changed name to "Cr---roads" so you can actually send it in their official server, have fun!
-
-You agree by using this script that i do not take responsibility for what you do with this. This paste is released for educational purposes so you can investigate the code
-You may also consider reporting the bug to Roblox VIA: https://www.roblox.com/support ( IF THEY GET A TON OF REQUESTS THEY WILL DO SOMETHING ABT IT ! )
-
-Let's hope they do it though lmao. Also, this has been a problem up for 7 years.
+Here's a gay ass leak lmao suck my cock also join discord.gg/skidding
 
 Proof: https://www.youtube.com/watch?v=8ELvmdF4slY || this is roblox showing that they actually knew about this issue
 ]]--   
